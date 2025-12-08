004adfb1e1224e8eebcadf5ad169d4448d4ca1f0
venkat ravilla <venkatravilla333@gmail.com>
2025-12-08 20:30:47 +0530

    initial commit

---
+++ 1.Basics/index.js
@@ -0,0 +1,111 @@
+
+
+// var a = 'sachin'
+// console.log(a)
+
+// var b = 40
+// console.log(b)
+
+// var c = false
+// console.log(c)
+
+// var d
+// console.log(d)
+
+// var e = null
+
+// console.log(e)
+
+//reference
+
+// var person = {
+//   name: 'sachin',
+//   age: 40,
+//   city: 'mumbai',
+//   play: () => {
+//     console.log('play')
+//   }
+// }
+
+// console.log(person)
+// console.log(person['name'])
+// console.log(person['age'])
+// console.log(person[city])
+// console.log(person.play())
+
+// var nums = [1, 2, 3, 4, 5, 'sachin']
+
+// console.log(nums)
+// console.log(nums[0])
+// console.log(nums[1])
+// console.log(nums[2])
+// console.log(nums[3])
+// console.log(nums[4])
+
+//declare
+
+// function test() {
+  
+// }
+
+// test()
+
+//expression
+
+// named
+
+// var x = function test() {
+  
+// }
+// x()
+
+//anonymous
+
+// var y = function () {
+  
+// }
+
+// y()
+
+//arrow
+
+// var z = ()=> {
+  
+// }
+
+// var a = 10
+
+// function b() {
+//   var a = 100
+// }
+// b()
+
+var a = 'sachin'
+
+console.log(a, typeof a)
+
+var b = 40
+console.log(b, typeof b)
+
+var c = true
+console.log(c, typeof c)
+
+var d
+console.log(d, typeof d)
+
+var e = null
+console.log(e, typeof e)
+
+var f = { name: 'kohli' }
+console.log(f, typeof f)
+
+var g = [1, 2, 3, 4, 5]
+console.log(g, typeof g, Array.isArray(g))
+
+var h = () => {
+  console.log('arrow fun')
+}
+console.log(h, typeof h)
+
+
+
---
+++ 2.Operators/index.js
@@ -0,0 +1,102 @@
+
+
+var a = 20
+
+var b = 10
+
+//arithmatic operators
+
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(a % b)
+// console.log(a ** b)
+
+//incremental
+// console.log(a)
+// console.log(++a) //pre
+// console.log(++a) //pre
+// console.log(a++) //post
+// console.log(a++) //post
+// console.log(a)
+
+//decremental
+// console.log(a)
+// console.log(--a) //pre
+// console.log(a--) //pre
+// console.log(a)
+
+//assignment operators
+
+// var a = 20
+
+// console.log(a)
+// console.log(++a)
+// console.log(++a)
+// console.log(++a)
+// console.log(++a)
+// console.log(++a)
+
+// console.log(a += 5)
+// console.log(a -= 5)
+// console.log(a *= 5)
+// console.log(a /= 5)
+// console.log(a %= 5)
+// console.log(a **= 5)
+
+
+//comparision operators
+
+// var a = 10
+
+// var b = 11
+//ralational operators
+
+// console.log(a > b)
+// console.log(a >= b)
+// console.log(a < b)
+// console.log(a <= b)
+
+//equality
+
+// var a = 10
+// var b = 11
+// var b = 10
+// var b = '10'
+
+// var a = undefined
+// var b = null
+
+// var a = {}
+// var b = {}
+// var b = a
+
+// var a = [1, 2, 3]
+
+// var b = [1, 2, 3]
+// var b = a
+
+// var a = ()=>{}
+// var b = ()=>{}
+// var b = a
+
+// var a = 10
+
+// var b = {}
+
+// var a = 10
+
+// var b = 10
+
+// console.log(a == b) //loose
+// console.log(a === b) //strict
+// console.log(a != b) //loose inequality
+// console.log(a !== b) //strict inequality
+
+
+
+
+
+
