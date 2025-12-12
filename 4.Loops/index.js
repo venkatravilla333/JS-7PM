38f330c5187850b26e21316311e4dd89f76bb84c
venkat ravilla <venkatravilla333@gmail.com>
2025-12-12 20:42:41 +0530

    loops

---
+++ 4.Loops/index.js
@@ -0,0 +1,255 @@
+
+
+// var age = new Number(40)
+var age = 40
+
+//without using loop
+
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+// console.log(age)
+
+
+//with using loop
+
+// console.log(age)
+//for loop
+
+// for (var i = 1; i >0 ; i++){
+//   console.log(i, age)
+// }
+
+// for (var i = 1; i<=10 ; i++){
+//   // if (i === 5) break
+//   // if (i === 5) continue
+//   console.log(i)
+// }
+
+
+
+// while loop
+
+// var i = 1
+// while (i<=10) {
+//   console.log(i, age)
+//   i++
+// }
+
+// var i = 11
+
+// do {
+//   console.log(i, age)
+//   i++
+// } while (i<=10) {
+  
+// }
+
+// var arr = [1, 2, 3, 4, 5]
+
+//without using loop
+
+// console.log(arr)
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+// console.log(arr[4])
+
+// for (var i = 0; i < arr.length; i++){
+//   console.log(i, arr[i])
+// }
+
+// console.log(arr)
+
+// for (var value of arr) {
+//   console.log(value)
+// }
+
+
+
+var names = ['sachin', 'dhoni', 'kohli', 'gill']
+
+//without uisng loop
+
+// console.log(names[0])
+// console.log(names[1])
+// console.log(names[2])
+// console.log(names[3])
+
+// for (var i = 0; i < names.length; i++){
+//   console.log(i, names[i])
+// }
+
+// for (var value of names){
+//   console.log(value)
+// }
+
+// var msg = 'Hi how are you'
+// console.log(msg.length)
+
+// for (var i = 0; i < msg.length; i++){
+//   console.log(i, msg[i])
+// }
+
+// for (var value of msg ){
+//   console.log(value)
+// }
+
+
+// var person = {
+//   name: 'sachin',
+//   age: 40,
+//   color: 'white',
+//   height: 5
+// }
+
+// console.log(person)
+
+// for (var value of person) {
+//   console.log(value)
+// }
+//without using loop
+
+// console.log(person)
+// console.log(person.name)
+// console.log(person['age'])
+// console.log(person.color)
+// console.log(person.height)
+
+
+//with using loop
+
+// for (var x in person) {
+//   console.log(x, person[x])
+// }
+
+
+var arr = [1, 2, 3, 4, 5]
+console.log(arr)
+
+// var iteratorObj = arr[Symbol.iterator]()
+
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+// console.log(iteratorObj.next())
+
+// for (var value in arr) {
+//   console.log(value)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
