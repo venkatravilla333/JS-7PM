42645eaded1953b0dcd291ceb74a0cd29233ffcb
venkat ravilla <venkatravilla333@gmail.com>
2026-01-06 20:19:48 +0530

    async js

---
+++ 9.Async-JS/index.js
@@ -0,0 +1,25 @@
+
+
+// console.log('one')
+
+// setTimeout(() => {
+//   console.log('timer-1')
+// }, 1000)
+
+// console.log('three')
+
+// setTimeout(() => {
+//   console.log('timer-2')
+// })
+// console.log('four')
+
+// Promise.resolve().then(() => console.log('pr-1')).then(() => console.log('pr-2'))
+
+// console.log('five')
+
+
+
+
+
+
+
